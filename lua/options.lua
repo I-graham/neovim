c8f0bc7f1720f6eaa349ecb75c0964d738a3066e
Ian Graham Martinez <iangraham28@gmail.com>
2025-04-10 16:42:42 -0600

    Fixed Rust LSP

--- lua/options.lua
+++ lua/options.lua
@@ -7,4 +7,4 @@ require "nvchad.options"
 local o = vim.o
 o.cursorlineopt ='both' -- to enable cursorline!
 
-o.scrolloff = 8
+o.scrolloff = 10
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -96,7 +96,7 @@ return {
       "debugloop/telescope-undo.nvim",
     },
     config = function()
-      require("telescope").setup {} 
+      require("telescope").setup {}
       require("telescope").load_extension "undo"
       vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>")
     end,
@@ -167,5 +167,27 @@ return {
     "chentoast/marks.nvim",
     event = "VeryLazy",
     opts = {},
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    lazy = false,
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("treesitter-context").setup {
+        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+        multiwindow = false, -- Enable multiwindow support.
+        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+        line_numbers = true,
+        multiline_threshold = 20, -- Maximum number of lines to show for a single context
+        trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+        mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+        -- Separator between context and content. Should be a single character string, like '-'.
+        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+        separator = "-",
+        zindex = 20, -- The Z-index of the context window
+        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+      }
+    end,
   },
 }
